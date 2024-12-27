169c30ec110a1931b998f41109c1a184f480dfdb
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-27 14:50:55 -0300

    27DEZ24 - Aula 203 AS - App ToDo (Disposição inicial da aplicação -> Start & Resources).

---
+++ app/src/main/java/com/arttt95/todo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.arttt95.todo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.arttt95.todo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = Intent(this, AdicionarTarefaActivity::class.java)
+
+        with(binding) {
+            fabAdicionar.setOnClickListener {
+
+                startActivity(intent)
+
+            }
+        }
+
+    }
+}
