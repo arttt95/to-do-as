b18233d3e395854f8a678a98e9f5268a6bb11090
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-28 17:15:31 -0300

    28DEZ24 - Aula 205 AS - Listando tarefas (No RecyclerView yet).

--- app/src/main/java/com/arttt95/todo/MainActivity.kt
+++ app/src/main/java/com/arttt95/todo/MainActivity.kt
@@ -2,17 +2,22 @@ package com.arttt95.todo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arttt95.todo.database.TarefaDAO
 import com.arttt95.todo.databinding.ActivityMainBinding
+import com.arttt95.todo.model.Tarefa
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var listaTarefas = emptyList<Tarefa>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +31,31 @@ class MainActivity : AppCompatActivity() {
 
         val intent = Intent(this, AdicionarTarefaActivity::class.java)
 
-        with(binding) {
+
+
+            with(binding) {
             fabAdicionar.setOnClickListener {
 
                 startActivity(intent)
 
             }
+
+
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val tarefaDAO = TarefaDAO(this)
+
+        listaTarefas = tarefaDAO.listar()
+
+        listaTarefas.forEach { tarefa ->
+
+            Log.i("info_db", "ID: ${tarefa.idTarefa} | Descrição: ${tarefa.descricao} | Data: ${tarefa.dataCadastro}\n")
+
         }
 
     }
--- app/src/main/java/com/arttt95/todo/database/TarefaDAO.kt
+++ app/src/main/java/com/arttt95/todo/database/TarefaDAO.kt
@@ -16,8 +16,6 @@ class TarefaDAO(context: Context) : ITarefaDAO {
 
     override fun salvar(tarefa: Tarefa): Boolean {
 
-//        val descricao =
-
         val valores = ContentValues()
         valores.put(DatabaseHelper.COLUMN_DESCRICAO, tarefa.descricao)
 
@@ -32,7 +30,7 @@ class TarefaDAO(context: Context) : ITarefaDAO {
             Log.i("info_db", "Sucesso: SALVAR tarefa -> TarefaDAO")
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.i("info_db", "Erro: SALVAR tarefa -> TarefaDAO")
+            Log.i("info_db", "Err: SALVAR tarefa -> TarefaDAO")
             return false
         }
 
@@ -49,6 +47,58 @@ class TarefaDAO(context: Context) : ITarefaDAO {
     }
 
     override fun listar(): List<Tarefa> {
-        TODO("Not yet implemented")
+
+        val listaTarefas = mutableListOf<Tarefa>()
+
+        val sql =
+            "SELECT " +
+                "${DatabaseHelper.COLUMN_ID_TAREFA}," + // id_tarefa
+                "${DatabaseHelper.COLUMN_DESCRICAO}," + // descricao
+
+                "strftime('%d/%m/%Y %H:%M', ${DatabaseHelper.COLUMN_DATA_CADASTRO}) " + // data
+                "AS ${DatabaseHelper.COLUMN_DATA_CADASTRO} " +
+
+            "FROM " +
+                "${DatabaseHelper.NOME_TB_TAREFAS};"
+
+        val cursor = reader.rawQuery(sql, null)
+
+        try {
+
+            if(cursor.moveToFirst()) {
+
+                do {
+                    val indiceId = cursor.getColumnIndex(DatabaseHelper.COLUMN_ID_TAREFA)
+                    val indiceDescricao = cursor.getColumnIndex(DatabaseHelper.COLUMN_DESCRICAO)
+                    val indiceDataCadastro = cursor.getColumnIndex(DatabaseHelper.COLUMN_DATA_CADASTRO)
+
+                    if (indiceId != -1 && indiceDescricao != -1 && indiceDataCadastro != -1) {
+
+                        val idTarefa = cursor.getInt(indiceId)
+                        val descricao = cursor.getString(indiceDescricao)
+                        val dataCadastro = cursor.getString(indiceDataCadastro)
+
+                        val tarefa = Tarefa(idTarefa, descricao, dataCadastro)
+
+                        listaTarefas.add(tarefa)
+
+                    } else {
+                        Log.i("info_db", "Err: inst tarefa LISTAR -> TarefaDAO")
+                    }
+                } while (cursor.moveToNext())
+
+            }
+
+            Log.i("info_db", "Sucesso: LISTAR tarefa -> TarefaDAO")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i("info_db", "Err: LISTAR tarefa -> TarefaDAO: ${e.message}")
+        } finally {
+            cursor.close()
+        }
+
+        return listaTarefas
+
     }
 }
